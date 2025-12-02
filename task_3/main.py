3b53a1dfda650176722bcdfedc8a01a0d6693772
Marharyta <margokesko@gmail.com>
2025-12-02 14:54:06 +0300

    Update main.py

--- task_3/main.py
+++ task_3/main.py
@@ -1,5 +1,5 @@
 #Кешко Маргарита
-with open('text.txt') as file: # открываем файл
+with open('C:\Users\User\OneDrive\Desktop\іпо\лр5\task_3\text.txt') as file: # открываем файл
     text = file.read()#читаем весь текст с файла
 words = text.split()#разбиваем текст на слова с пробелом как разделителем
 print(f"Количество слов в текстовом файле: {len(words)}")#
